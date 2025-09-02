fbf9e27a31d370e717f5156a1f75f5f9aea45ae9
sebastian.redemann@campus.tu-berlin.de <sebastian.redemann@campus.tu-berlin.de>
2025-09-02 16:08:41 +0200

    Praktikum 1, Wuchten mit 1 FG

--- Uebung_1.m
+++ Uebung_1.m
@@ -1,47 +1,53 @@
-load("awt_UE1_Aufgabe.mat");
+%load("Null_Lauf.mat");
+%% Daten aufnehmen
+Null_Lauf=awt_messen("COM5", 5);
+%% Daten aufnehmen
+Eins_Lauf=awt_messen("COM5", 5); % Testgewicht angebracht.
 
-accel_1 = awt_UE1(:,1);
-accel_2 = awt_UE1(:,2);
-trigger = awt_UE1(:,3);
-time = awt_UE1(:,4); % in µs
+%% Zeitwerte kalkulieren
+function [t, f_s] = calculate_time(data)
+    % data: Matrix aus Null_Lauf.mat (mit 4. Spalte = Zeit in µs)
 
-t_end = sum(time)*10e-6; % in s
+    time = data(:,4);            % Zeitspalte
+    t_end = sum(time) * 1e-6;    % in s (Achtung: 10e-6 ist 1e-5, nicht 1e-6!)
+    delta_t = mean(time) * 1e-6; % Abtastschritt in s
+    f_s = 1/delta_t;             % Samplingfrequenz in Hz
 
-delta_t = mean(awt_UE1(:,4));
+    t = linspace(0, t_end, length(time));
+end
 
-f_s = 1/delta_t; % in Hz
-
-t = linspace(0, t_end, 3275);
-
+[t_a_0, f_s] = calculate_time(Null_Lauf);
 %% Plotting raw data
 
+figure(1)
 tiledlayout(3,1)
 
 nexttile
 fig1 = figure(1);
 
-plot(t, accel_1);
+plot(t_a_0, Null_Lauf(:,1));
 title("Beschleunigung 1")
 
 nexttile
-plot(t, accel_2);
+plot(t, Null_Lauf(:,2));
 title("Beschleunigung 2")
 
 nexttile
-plot(t, trigger);
+plot(t, Null_Lauf(:,3));
 title("Triggerfunktion")
 
-%% Zuschnitt Triggerfunktion
-diff_trigg = diff(trigger); % diff function for peak finding
-first_index = find(diff_trigg == 1, 1, "first") + 1;
-last_index = find(diff_trigg == 1, 1, "last"); % index of last non zero element
+%% Zuschnitt Triggerfunktion und Beschleunigungen
+function [accel_1_cut, accel_2_cut, trigger_cut, t_cut] = cut_signals(accel_1, accel_2, trigger, t)
+    diff_trigg = diff(trigger);
+    first_index = find(diff_trigg == 1, 1, "first") + 1;
+    last_index = find(diff_trigg == 1, 1, "last");
 
-trigger_cut = trigger(first_index:last_index);
-t_cut = t(1: length(trigger_cut)); %
+    trigger_cut = trigger(first_index:last_index);
+    accel_1_cut = accel_1(first_index:last_index);
+    accel_2_cut = accel_2(first_index:last_index);
+    t_cut = t(1:length(trigger_cut));
+end
 
-%% Zuschnitt Beschleunigung
-accel_1_cut = accel_1(first_index:last_index);
-accel_2_cut = accel_2(first_index:last_index);
 
 %% Plot der zugeschnittenen Signale
 tiledlayout(3, 1)
@@ -61,32 +67,43 @@ plot(t_cut, trigger_cut);
 title("geschnittenes Triggersignal")
 
 %% FFT der Signale
-L = length(t_cut); % Length of signal
+function [freq_index, amp, angle_deg] = signal_fft(signal, f_s)
+    L = length(signal);
+    fft_signal = 2*fft(signal)/L;
 
-fft_trigger = 2*fft(trigger_cut)/L;
+    % Max. Amplitude (ignoriere DC-Anteil)
+    amp = max(abs(fft_signal(2:end/2)));
 
-fig_7 = figure(7);
-plot(f_s/L*(0:L-1), abs(fft_trigger))
+    % Index des Maximums
+    freq_index = find(abs(fft_signal) == amp, 1);
 
-trigger_amplitude = max(abs(fft_trigger(2:end/2)));
-trigger_index = find(abs(fft_trigger) == trigger_amplitude);
-trigger_index = trigger_index(1);
-
-fft_accel_1 = 2*fft(accel_1_cut)/L;
-accel_1_frequency = fft_accel_1(trigger_index+1);
-accel_1_amplitude = max(abs(fft_accel_1(2:end/2)));
-accel_1_angle = angle(fft_accel_1(trigger_index))*180/pi;
-
-fft_accel_2 = 2*fft(accel_2_cut)/L;
-accel_2_frequency = fft_accel_2(trigger_index+1);
-accel_2_amplitude = max(abs(fft_accel_2(2:end/2)));
-accel_2_angle = angle(fft_accel_2(trigger_index))*180/pi;
+    % Phasenwinkel in Grad
+    angle_deg = angle(fft_signal(freq_index)) * 180/pi;
+end
 
 %% 3D Plot der Fourier Signale
+plot()
 
-
+%% Abfrage nach positivem Massenausgleich
+% answer = questdlg('Möchten Sie einen positiven Massenausgleich?', ...
+% 	'Dessert Menu', ...
+% 	'Ice cream','Cake','No thank you','No thank you');
+% % Handle response
+% switch answer
+%     case 'Ja'
+%         disp([answer ' Es wird ein positiver Massenausgleich verwendet.'])
+%         dessert = 1;
+%     case 'Cake'
+%         disp([answer ' coming right up.'])
+%         dessert = 2;
+%     case 'No thank you'
+%         disp('I''ll bring you your check.')
+%         dessert = 0;
+% end
 
 %% Plot Polarcoordinates
+% Option für Einstellen von Winkeln an denen tatsächlich ein Setzungsgewicht anbringbar ist, z.B. bei vorgegebenen Löchern.
+% Option für Wiedergabe von negativem Massenausgleich => 180 Grad versetzt
 
 fig_8 = figure(8);
 polarplot([0 accel_1_angle/180*pi], [0 accel_1_amplitude], "black-o", "DisplayName", "Beschleunigungssensor 1");
@@ -97,3 +114,18 @@ legend show
 
 %% instationäre Drehzahlen
 stft(trigger_cut, f_s, Window=kaiser(256,5),OverlapLength=220,FFTLength=512)
+
+%% U_test in komplexe Zahl umrechnen
+gewicht = 3,6; % in gramm
+winkel = 90; % in Grad
+
+betrag = gewicht*cosd(winkel);
+komplexer_winkel = gewicht*sind(winkel);
+
+u_test = betrag + 1i*komplexer_winkel;
+u_test;
+
+% b_hat = (accel_1_frequency - (-0,1128+1i*0,036))/u_test;
+% u_wuchtsetzung = - (-0,1128+1i*0,036/b_hat);
+
+
