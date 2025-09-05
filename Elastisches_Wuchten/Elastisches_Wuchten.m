b00330edd32de43b61f9b7dffcf432da036cd620
sebastian.redemann@campus.tu-berlin.de <sebastian.redemann@campus.tu-berlin.de>
2025-09-05 11:25:04 +0200

    Modales Wuchten fertig für eine Eigenform

--- Elastisches_Wuchten/Elastisches_Wuchten.m
+++ Elastisches_Wuchten/Elastisches_Wuchten.m
@@ -1,40 +1,29 @@
-tmp = load("awt_UE1_Aufgabe.mat", "awt_UE1");
-null_lauf = tmp.awt_UE1;
-%% Abfrage nach positivem Massenausgleich
-% answer = questdlg('Möchten Sie einen positiven Massenausgleich?', ...
-% 	'Dessert Menu', ...
-% 	'Ice cream','Cake','No thank you','No thank you');
-% % Handle response
-% switch answer
-%     case 'Ja'
-%         disp([answer ' Es wird ein positiver Massenausgleich verwendet.'])
-%         dessert = 1;
-%     case 'Cake'
-%         disp([answer ' coming right up.'])
-%         dessert = 2;
-%     case 'No thank you'
-%         disp('I''ll bring you your check.')
-%         dessert = 0;
-% end
+tmp = load("awt_el_neu.mat", "Fs_0", "mode1_0", "mode1_1", "mode2_0", "mode2_1");
+% [Fs_0, null_lauf_mode_1, ein_lauf_mode_1, null_lauf_mode_2, ein_lauf_mode_2] = tmp.Fs_0;
+f_s_0 = tmp.Fs_0;
+null_lauf_mode_1 = tmp.mode1_0;
+ein_lauf_mode_1 = tmp.mode1_1;
+null_lauf_mode_2 = tmp.mode2_0;
+ein_lauf_mode_2 = tmp.mode2_1;
 
-%% Daten aufnehmen
-% null_lauf = awt_messen("COM5", 5);
+%% Timetable to array
+null_lauf_mode_1 = table2array(null_lauf_mode_1(:, [2,3,1]));
+ein_lauf_mode_1 = table2array(ein_lauf_mode_1(:, [2,3,1]));
+null_lauf_mode_2 = table2array(null_lauf_mode_2(:, [2,3,1]));
+ein_lauf_mode_2 = table2array(ein_lauf_mode_2(:, [2,3,1]));
 
-%% Daten Laden
-% tmp = load("Null_Lauf.mat", "null_lauf");
-% null_lauf = tmp.null_lauf;
-% tmp = load("Urunwucht_UE2.mat", "data");
-% null_lauf = tmp.data();
-
-%% Zeitwerte kalkulieren
-[t_0, f_s_0] = calculate_time(null_lauf);
+% Datensatz zerlegen
+data1_0 = null_lauf_mode_1(:,1);
+data2_0 = null_lauf_mode_1(:,2);
+tr_0 = null_lauf_mode_1(:,3);
+t_0 = linspace(0, length(data1_0)/f_s_0, length(data1_0));
 
 %% Zuschnitt
 [accel_1_cut_0, accel_2_cut_0, trigger_cut_0, t_cut_0] = ...
-    cut_signals(null_lauf(:,1), null_lauf(:,2), null_lauf(:,3), t_0); % Null-Lauf geschnitten
+    cut_signals(null_lauf_mode_1(:,1), null_lauf_mode_1(:,2), null_lauf_mode_1(:,3), t_0); % Null-Lauf geschnitten
 
 %% Plotting raw data
-plot_raw_data(null_lauf, t_0, "(0-Lauf)");
+plot_raw_data(null_lauf_mode_1, t_0, "(0-Lauf)");
 
 %% Plotting cut data
 plot_cut_data(accel_1_cut_0, accel_2_cut_0, trigger_cut_0, t_cut_0, "(0-Lauf) geschnitten");
@@ -63,28 +52,28 @@ accel_2_0 = accel_2_0_amp + 1i*accel_2_0_angle;
 %% Polar coordinate plot
 plot_polarcoordinates(accel_1_0_amp, accel_1_0_angle, accel_2_0_amp, accel_2_0_angle);
 
+%% Bestimmung Urunwucht u_10
+
+
 %% Umrechnung in komplexe Werte
 testsetzung_gewicht = 3.38;
 testsetzung_winkel = 0;
 
 u_test = testsetzung(testsetzung_gewicht, testsetzung_winkel);
 
-%% Testsetzung Daten aufnehmen
-% ein_lauf=awt_messen("COM5", 5); % Testgewicht angebracht, position in Winkel und Masse dokumentiert
-
-%% Daten laden
-tmp_2 = load("awt_UE1_Aufgabe.mat", "awt_UE1");
-ein_lauf = tmp_2.awt_UE1;
-
-%% Zeitwerte kalkulieren
-[t_1, f_s_1] = calculate_time(ein_lauf);
+%% Datensatz zerlegen
+data1_1 = ein_lauf_mode_1(:,1);
+data2_1 = ein_lauf_mode_1(:,2);
+tr_1 = ein_lauf_mode_1(:,3);
+t_1 = linspace(0, length(data1_0)/f_s_0, length(data1_0));
+f_s_1 = f_s_0;
 
 %% Zuschnitt
 [accel_1_cut_1, accel_2_cut_1, trigger_cut_1_1, t_cut_1] = ...
-    cut_signals(ein_lauf(:,1), ein_lauf(:,2), ein_lauf(:,3), t_1); % Ein-Lauf geschnitten
+    cut_signals(ein_lauf_mode_1(:,1), ein_lauf_mode_1(:,2), ein_lauf_mode_1(:,3), t_1); % Ein-Lauf geschnitten
 
 %% Plotting raw data
-plot_raw_data(ein_lauf, t_1,  "(1-Lauf)");
+plot_raw_data(ein_lauf_mode_1, t_1,  "(1-Lauf)");
 
 %% Plotting cut data
 plot_cut_data(accel_1_cut_1, accel_2_cut_1, trigger_cut_1_1, t_cut_1, "(1-Lauf) geschnitten");
@@ -112,13 +101,112 @@ accel_2_1 = accel_2_1_amp + 1i*accel_1_1_angle;
 % Zuschnitt 1 Lauf auf gleiche Größe wie 0 Lauf!
 
 %% Berechnung b
-b_hat = (accel_1_1 - accel_1_0)/u_test;
+u_hat_1_0 = -(accel_1_0)/(accel_1_1 - accel_1_0);
+t_1_norm = [1, 1];
+t_1_A = u_hat_1_0 * t_1_norm;
 
-%% Berechnung Wuchsetzung u_hat
-u_hat = - (accel_1_0/b_hat);
 
 %% Von Komplex zu real
-[gewicht_1, winkel_1] = complex_to_real(u_hat);
+[gewicht_1, winkel_1] = complex_to_real(u_hat_1_0);
+antwort = ["Die Auswuchtsetzung beträgt", gewicht_1, " gramm.", "Der Winkel unter dem die Auswuchtsetzung angebracht werden muss beträgt: ", winkel_1];
+disp(antwort);
+%% plot Polar 
+fig8 = figure(8);
+
+compassplot([0 accel_1_0_angle/180*pi], [0 accel_1_0_amp], "black-o", "DisplayName", "r_0");
+legend show
+
+%% Zweite Eigenform berechnen
+
+% Datensatz zerlegen
+data1_2 = null_lauf_mode_2(:,1);
+data2_2 = null_lauf_mode_2(:,2);
+tr_2 = null_lauf_mode_2(:,3);
+t_2 = linspace(0, length(data1_0)/f_s_0, length(data1_0));
+
+%% Zuschnitt
+[accel_1_cut_2, accel_2_cut_2, trigger_cut_2, t_cut_2] = ...
+    cut_signals(null_lauf_mode_2(:,1), null_lauf_mode_2(:,2), null_lauf_mode_2(:,3), t_2); % Null-Lauf geschnitten
+
+%% Plotting raw data
+plot_raw_data(null_lauf_mode_2, t_2, "(0-Lauf)");
+
+%% Plotting cut data
+plot_cut_data(accel_1_cut_2, accel_2_cut_2, trigger_cut_2, t_cut_2, "(0-Lauf) geschnitten");
+
+%% FFT
+% FFT für alle Signale erstellen
+% Amplitude von Triggerfunktion aus dem Frequenzbereich bestimmen
+% Index der Amplitude der Triggerfunktion finden
+% Den Index auf die Beschleunigungen im Frequenzbereich anwenden und dort
+% die Werte auslesen
+[trigger_2_fft, trigger_cut_2_index] = trigger_data(trigger_cut_2);
+[accel_1_2_fft, accel_1_2_amp, accel_1_2_angle] = create_fft(accel_1_cut_2, trigger_cut_2_index);
+[accel_2_2_fft, accel_2_2_amp, accel_2_2_angle] = create_fft(accel_2_cut_2, trigger_cut_2_index);
+
+%% FFT plotting
+[accel_1_2_fft_plot] = plot_fft(accel_1_2_fft, f_s_0, "Beschleunigung 1 (0-Lauf)");
+[accel_2_2_fft_plot] = plot_fft(accel_2_2_fft, f_s_0, "Beschleunigung 2 (0-Lauf)");
+[trigger_2_fft_plot] = plot_fft(trigger_2_fft, f_s_0, "Triggerfunktion (0-Lauf)");
+
+%% 3D Plot der Fourier Signale
+% tiled(trigger_fft_plot, accel_1_0_fft_plot, accel_2_0_fft_plot, "0-Lauf");
+%% Komplexe Zahlen bilden
+
+accel_1_2 = accel_1_2_amp + 1i*accel_1_2_angle;
+accel_2_2 = accel_2_2_amp + 1i*accel_2_2_angle;
+%% Polar coordinate plot
+plot_polarcoordinates(accel_1_2_amp, accel_1_2_angle, accel_2_2_amp, accel_2_2_angle);
+
+%% Zweite Eigenform eins_lauf
+
+%% Datensatz zerlegen
+data1_1 = ein_lauf_mode_1(:,1);
+data2_1 = ein_lauf_mode_1(:,2);
+tr_1 = ein_lauf_mode_1(:,3);
+t_1 = linspace(0, length(data1_0)/f_s_0, length(data1_0));
+f_s_1 = f_s_0;
+
+%% Zuschnitt
+[accel_1_cut_1, accel_2_cut_1, trigger_cut_1_1, t_cut_1] = ...
+    cut_signals(ein_lauf_mode_1(:,1), ein_lauf_mode_1(:,2), ein_lauf_mode_1(:,3), t_1); % Ein-Lauf geschnitten
+
+%% Plotting raw data
+plot_raw_data(ein_lauf_mode_1, t_1,  "(1-Lauf)");
+
+%% Plotting cut data
+plot_cut_data(accel_1_cut_1, accel_2_cut_1, trigger_cut_1_1, t_cut_1, "(1-Lauf) geschnitten");
+
+%% FFT
+% FFT für alle Signale erstellen
+% Amplitude von Triggerfunktion aus dem Frequenzbereich bestimmen
+% Index der Amplitude der Triggerfunktion finden
+% Den Index auf die Beschleunigungen im Frequenzbereich anwenden und dort
+% die Werte auslesen
+[trigger_1_1_fft, trigger_cut_1_1_index] = trigger_data(trigger_cut_1_1);
+[accel_1_1_fft, accel_1_1_amp, accel_1_1_angle] = create_fft(accel_1_cut_1, trigger_cut_1_1_index);
+[accel_2_1_fft, accel_2_1_amp, accel_2_1_angle] = create_fft(accel_2_cut_1, trigger_cut_1_1_index);
+
+%% FFT plotting
+[accel_1_1_fft_plot] = plot_fft(accel_1_1_fft, f_s_1, "Beschleunigung 1 (1-Lauf)");
+[accel_2_1_fft_plot] = plot_fft(accel_2_1_fft, f_s_1, "Beschleunigung 2 (1-Lauf)");
+[trigger_1_1_fft_plot] = plot_fft(trigger_1_1_fft, f_s_1, "Triggerfunktion (1-Lauf)");
+
+%% Komplexe Zahl bilden
+
+accel_1_1 = accel_1_1_amp + 1i*accel_1_1_angle;
+accel_2_1 = accel_2_1_amp + 1i*accel_1_1_angle;
+%% Plot FFT 0-Lauf zu 1-Lauf
+% Zuschnitt 1 Lauf auf gleiche Größe wie 0 Lauf!
+
+%% Berechnung b
+u_hat_1_0 = -(accel_1_0)/(accel_1_1 - accel_1_0);
+t_1_norm = [1, 1];
+t_1_A = u_hat_1_0 * t_1_norm;
+
+
+%% Von Komplex zu real
+[gewicht_1, winkel_1] = complex_to_real(u_hat_1_0);
 
 %% plot Polar 
 fig8 = figure(8);
@@ -130,6 +218,7 @@ hold on
 polarplot([accel_1_0_angle/180*pi accel_1_1_angle/180*pi], [accel_1_0_amp accel_1_1_amp], "magenta-o", "DisplayName", "differenz");
 hold off
 legend show
+
 %% ====== FUNKTIONEN ======
 function [t, f_s] = calculate_time(data)
     time = data(:,4);                 % Zeitspalte (µs)
@@ -142,7 +231,7 @@ end
 function [accel_1_cut, accel_2_cut, trigger_cut, t_cut] = cut_signals(accel_1, accel_2, trigger, t)
     diff_trigg = diff(trigger);
     first_index = find(diff_trigg > 0.5, 1, "first") + 1;
-    last_index  = find(diff_trigg == 1, 1, "last");
+    last_index  = find(diff_trigg > 0.5, 1, "last");
 
     trigger_cut = trigger(first_index:last_index);
     accel_1_cut = accel_1(first_index:last_index);
